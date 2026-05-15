3d37bc3c8001cbaa8bbd7def750fa91c8aaf71e9
Vishnu <vishnubadiger30@gmail.com>
2026-05-15 06:16:00 +0530

    Add solution for finding minimum in rotated array
    Time Complexity
    Binary Search → O(log n)
    Space Complexity
    O(1)

---
+++ BinarySearch/153 - Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,15 @@
+# LeetCode 153 - Find Minimum in Rotated Sorted Array
+
+class Solution:
+    def findMin(self, nums):
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return nums[left]
