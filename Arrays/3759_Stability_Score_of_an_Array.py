e956476d4289e0a59049fc700cc86e6e557f27d4
Vishnu <vishnubadiger30@gmail.com>
2026-09-06 06:53:11 +0530

    Add firstStableIndex method to Solution class
    Implement a method to find the first stable index in an array based on a given threshold.
    Complexity
    Time: O(n)
    Space: O(n)

---
+++ Arrays/3759_Stability_Score_of_an_Array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+
+        # suffix_min[i] = minimum value from i to n-1
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+
+        # Find the first stable index
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+
+            if prefix_max - suffix_min[i] <= k:
+                return i
+
+        return -1
